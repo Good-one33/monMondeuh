3f69d80cbf3e4f0aae082262c08f3be34dfba900
Abdelmalek <benali.abdelmalek63@gmail.com>
2022-10-04 23:16:50 +0200

    let's keep eval fot now and find better solution later

---
+++ JS/script_1.js
@@ -0,0 +1,16 @@
+let f1 = ()=>{
+    console.log('f1')
+    document.querySelector('#jobStyle').setAttribute('href','css/s'+1+'.css')
+    jobDisplayDiv.innerHTML = `<button> red </button>
+    <button>blue</button> `
+
+    let jobDisplay = document.querySelector('.jobDisplay')
+        let btn = document.querySelectorAll('button')
+
+        btn[0].addEventListener('click', ()=> {
+            jobDisplay.classList.toggle('red')
+        })
+        btn[1].addEventListener('click', ()=> {
+            jobDisplay.classList.toggle('blue')
+        })
+}
