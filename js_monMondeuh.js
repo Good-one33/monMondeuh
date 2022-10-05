5471769848cb19c89cee4cb4f2caa6bf79be710d
Abdelmalek <benali.abdelmalek63@gmail.com>
2022-10-05 08:44:18 +0200

    adding job's script dynamically to the head

--- js_monMondeuh.js
+++ js_monMondeuh.js
@@ -3,27 +3,20 @@ const jobDisplayDiv = document.querySelector('.jobDisplay')
 
 
 
-let JobsRender = ()=>{
+
 let jobs = Array.from(jobsDiv.children)  //putting all job's divs in an array 
-jobs.forEach(job => {                      // adding some attributes to each item in the list 
-let jobId = job.getAttribute('id')  
-job.innerText = jobId // adding the function handler for each job's box , this will control evrything in the JobDisplay div
-job.addEventListener('click',e=>{
-
-    let element = document.createElement('div')
-    element.innerHTML = "<h1> Hello "+ e.target.getAttribute('id') +"</h1> "
-
-    jobDisplayDiv.appendChild(element)
-
-   eval('f'+e.target.getAttribute('id')+'()')
-   // eval is not the best option but I will keep it for now !
-    
+jobs.forEach(job => {  
+job.innerText =  job.getAttribute('id')    // adding some attributes to each item in the list 
+let script = document.createElement('script')
+document.head.appendChild( script).setAttribute('src','js/script_'+job.getAttribute('id') +'.js')
+ // adding the function handler for each job's box , this will control evrything in the JobDisplay div
+ job.addEventListener('click',e=>{
+eval('f'+e.target.getAttribute('id')+'()')
+// eval is not the best option but I will keep it for now !    
 })    
 
 })
-}
 
-JobsRender()
 
 
 
