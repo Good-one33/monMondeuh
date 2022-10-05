442e835303cce65ef2abd745da49533905da26f4
Abdelmalek <benali.abdelmalek63@gmail.com>
2022-10-05 23:18:25 +0200

    add some effects to the selected job

--- js_monMondeuh.js
+++ js_monMondeuh.js
@@ -2,17 +2,24 @@ const jobsDiv = document.querySelector('.jobs')
 const jobDisplayDiv = document.querySelector('.jobDisplay')
 
 
+/*putting all job's divs in an array
+  adding some attributes to each item in the list
+  dynamically adding a  script link to the  file  in js folder
+  adding the function handler for each job's box , this will control evrything in the JobDisplay div
+  looping through all jobs and remove the selected class and add it only to the selected one 
+*/
+let jobs = Array.from(jobsDiv.children)   
 
-
-let jobs = Array.from(jobsDiv.children)  //putting all job's divs in an array 
 jobs.forEach(job => {  
-job.innerText =  job.getAttribute('id')    // adding some attributes to each item in the list 
-let script = document.createElement('script')
-document.head.appendChild( script).setAttribute('src','js/script_'+job.getAttribute('id') +'.js')
- // adding the function handler for each job's box , this will control evrything in the JobDisplay div
- job.addEventListener('click',e=>{
-eval('f'+e.target.getAttribute('id')+'()')
-// eval is not the best option but I will keep it for now !    
+job.innerText =  job.id    
+document.head.appendChild(document.createElement('script')).setAttribute('src','js/script_'+job.id +'.js')
+
+job.addEventListener('click',e=>{
+eval('f'+e.target.id+'()') // eval is not the best option but I will keep it for now !  
+jobs.forEach((job) => {
+    job.classList.remove('selected')
+})
+e.target.classList.toggle('selected')
 })    
 
 })
