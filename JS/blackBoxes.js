4d8e03d475318a79e90cbdffd975038ba3869194
Abdelmalek <benali.abdelmalek63@gmail.com>
2023-02-20 09:14:51 +0100

    added animation into jobs :)

--- JS/blackBoxes.js
+++ JS/blackBoxes.js
@@ -2,7 +2,7 @@
 
 // loading the bodymoving animation 
 var animation = bodymovin.loadAnimation({
-  container: document.getElementById("bbWoelcomeSvgContainer"),
+  container: document.getElementById("bbWelcomeSvgContainer"),
   path: "source/Bodymovin/blackBoxWelcomeAnimation.json",
   renderer: "svg",
   loop: true,
@@ -10,8 +10,8 @@ var animation = bodymovin.loadAnimation({
   name: "Demo1 ",
 });
 
-const jobsDiv = document.querySelector('.jobs')
-const jobDisplayDiv = document.querySelector('.jobDisplay')
+let jobsDiv = document.querySelector('.jobs')
+let jobDisplayDiv = document.querySelector('.jobDisplay')
 /*putting all job's divs in an array
   adding some attributes to each item in the list
   dynamically adding a  script link to the  file  in js folder
@@ -21,8 +21,12 @@ const jobDisplayDiv = document.querySelector('.jobDisplay')
 let jobs = Array.from(jobsDiv.children)   
 
 jobs.forEach(job => {  
-job.innerText =  job.id    
+job.innerText =  job.id
+// if(job.id == 6){
+//   job.classList.add('red')
+// } 
 document.head.appendChild(document.createElement('script')).setAttribute('src','JS/modularJs/script_'+job.id +'.js')
+let child = document.createElement('div').classList.add('item')  
 
 job.addEventListener('click',e=>{ // each time we click on a job, we execute the function related to it 
 eval('f'+e.target.id+'()') // eval is not the best option but I will keep it for now !!!!!  
@@ -34,7 +38,84 @@ e.target.classList.toggle('selected')
 
 })
 
+var container = document.getElementById('item');
+var state = 'play';
 
 
+var animationTest = bodymovin.loadAnimation({
+  container: document.getElementById('item'),
+  path:"https://maxst.icons8.com/vue-static/landings/animated-icons/icons/pause/pause.json",
+  renderer: "svg",
+  loop: false,
+  autoplay: false,
+});
+animationTest.goToAndStop(14, true);
 
 
+container.addEventListener('click', () => {
+  if(state === 'play') {
+   animationTest.playSegments([14, 27], true);
+    state = 'pause';
+  } else {
+    animationTest.playSegments([0, 14], true);
+    state = 'play';
+  }
+});
+
+animationTest.addEventListener('DOMLoaded', () => {
+  const p = document.createElement('p');
+  p.textContent = 'The animation has just been loaded into the container. Thanks to the DOMLoaded event!';
+  container.after(p);
+});
+
+var count = 0;
+
+animationTest.addEventListener('complete', () => {
+  count++;
+  const p = document.createElement('p');
+  let text = `You have played the animation ${count} `;
+  let plural = (count === 1) ? 'time' : 'times';
+  text += `${plural}. Thanks to the complete event!`
+  
+  p.textContent = text;
+  
+  if(count !== 1) {
+    jobDisplayDiv.lastChild.textContent = text;
+  } else jobDisplayDiv.appendChild(p);
+})
+
+jobs.forEach(job => { 
+
+  // job.classList.add('red')
+  var container = document.getElementById(job.id);
+  var state = 'play';
+  let path = '';
+  if(job.id == 6 || job.id == 12 || job.id == 8 ){
+    path = "https://maxst.icons8.com/vue-static/landings/animated-icons/icons/settings/settings.json";
+  }else if (job.id == 7 || job.id == 1 || job.id == 5){
+    path = "https://maxst.icons8.com/vue-static/landings/animated-icons/icons/pause/pause.json";
+  }else if (job.id == 0 || job.id == 9 || job.id == 13){
+    path = "https://maxst.icons8.com/vue-static/landings/animated-icons/icons/book/book.json";
+  }else {
+    path = "https://maxst.icons8.com/vue-static/landings/animated-icons/icons/adjust/adjust.json";
+  }
+  var job6Animation = bodymovin.loadAnimation({
+    container: container,
+    path:path,                                                                              //"https://maxst.icons8.com/vue-static/landings/animated-icons/icons/pause/pause.json",
+    renderer: "svg",
+    loop: false,
+    autoplay: false,
+  });
+  job6Animation.goToAndStop(14, true);
+  container.addEventListener('click', () => {
+    if(state === 'play') {
+      job6Animation.playSegments([14, 27], true);
+      state = 'pause';
+    } else {
+      job6Animation.playSegments([0, 14], true);
+      state = 'play';
+    }
+  });
+
+
+ })
--- JS/modularJs/script_12.js
+++ JS/modularJs/script_12.js
@@ -1,20 +1,31 @@
 let f12 = ()=>{
     console.log('f12 is running')
     // this will load the style for the job 
-    document.querySelector('#jobStyle').setAttribute('href','CSS/modularCss/s'+6+'.css')   
+    document.querySelector('#jobStyle').setAttribute('href','CSS/modularCss/s'+12+'.css')   
     
     // this will load the html content
     jobDisplayDiv.innerHTML = `
-    <div class="icon-container">
-        <span class="eicon-security"></span>
+    <div class ="container">
+    <div id="item1"> </div>
+    <div id="item2"> </div>
     </div>
-    <div class="icon-container">
-        <span class="eicon-blocks"> test</span>
-    </div>
-    <div class="icon-container">
-        <span class="eicon-globe"></span>
-    </div>`
+    `
 
     // this is the js code running for this job 
+    let loop = false;
 
+    var animationTen = bodymovin.loadAnimation({
+        container: document.getElementById('item1'),
+        path: "https://assets7.lottiefiles.com/temp/lf20_D0nz3r.json",
+        renderer: "svg",
+        loop: loop,
+        autoplay: true,
+      });
+      var animationTen = bodymovin.loadAnimation({
+        container: document.getElementById('item2'),
+        path: "https://maxst.icons8.com/vue-static/landings/animated-icons/icons/book/book.json",
+        renderer: "svg",
+        loop: loop,
+        autoplay: true,
+      });
 }////////// don't delete me 
