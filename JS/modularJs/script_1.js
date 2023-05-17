009334201a86cfda56b61b88cc1f02680afbb040
Abdelmalek <benali.abdelmalek63@gmail.com>
2023-05-17 12:53:01 +0200

    marseille commit 1

--- JS/modularJs/script_1.js
+++ JS/modularJs/script_1.js
@@ -9,17 +9,17 @@ let f1 = () => {
   jobDisplayDiv.innerHTML = `
         Hello I am script one
         <div class="J1_inputContainer">
-        <input type="number" class="J1_input" >
-        <input type="number" class="J1_input" >
+        <input type="number" class="J1_input" min="0" max="100" value="10">
+        <input type="number" class="J1_input" min="1" max="100" value="2">
         </div>
         <div class="initialList"></div>
+        <div class="sublistsContainer"></div>
     `;
   //the code running for the job
-  //¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤ getSublists ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
-  // this function take an iniital list and returns another list containing sublist with a length defined in the second argument
+  //---------------------< getSublists >----------------this function take an iniital list and returns another list containing sublist with a length defined in the second argument
   function getSublists(initialList, subListLength) {
     let resultList = [];
-    console.log("initial List " + initialList);
+    //console.log("initial List " + initialList);
     let count = 0;
     let subListMax = subListLength; // in beggining it is equal to sublistLength but it will changes each loop 
     let maxNbrSubList;
@@ -48,10 +48,10 @@ let f1 = () => {
     resultList.forEach((subList) => {
       console.log(subList);
     });
-  }
 
-  //¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤ insertItemsToList ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
-  //funciton to create and insert elements to a list
+    return resultList;
+  }
+  //---------------------< insertItemsToList >----------funciton to create and insert elements to a list
   let insertItemsToList = (classListName, nbr) => {
     let list = document.querySelector("." + classListName);
     for (let i = 1; i <= nbr; i++) {
@@ -60,9 +60,7 @@ let f1 = () => {
       list.appendChild(listItem);
     }
   };
-
-  //¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤ emptyList ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
-  //funciton to create and insert elements to a list
+  //---------------------< emptyList >------------------funciton remove all  elements of a list  
   let emptyList = (classListName) => {
     let list = document.querySelector("." + classListName);
     let listItems =  [].slice.call(list.children);
@@ -71,24 +69,35 @@ let f1 = () => {
     })
   };
 
+  //¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤ main code
   // let objectIds = ["1", "2", "1", "2", "1", "2", "1", "2", "1", "2", "1", "2"];
-  // getSublists(objectIds, 3);
+  // getSublists(objectIds, 1);
   let inputs = document.querySelectorAll(".J1_input");
+  let tempList = [];
+  let sublistsContainer = document.querySelector('.sublistsContainer')
 
-  inputs[0].addEventListener("change", () => {
+  inputs[0].addEventListener("click", () => {
     emptyList("initialList");
-    insertItemsToList("initialList", inputs[0].value);  
+    insertItemsToList("initialList",parseInt(inputs[0].value));  
+    tempList = Array.from(document.querySelector(".initialList").children)
+  });
+
+  inputs[1].addEventListener("click", () => {
+    emptyList("sublistsContainer");
+    console.log('sublistsContainer : befor'+ sublistsContainer)
+    let slicedList = getSublists(tempList, parseInt(inputs[1].value)); 
+    console.log(slicedList)
+    slicedList.forEach((subList) =>{
+      const tempSubList = document.createElement('div');     
+      tempSubList.classList.add('subList');
+      for(let i=0; i<subList.length;i++){
+        tempSubList.appendChild(subList[i])
+      }
+      sublistsContainer.appendChild(tempSubList)
+    })
+    console.log('sublistsContainer : after'+ sublistsContainer)
 
   });
 
-  inputs[1].addEventListener("change", () => {
-    let list = document.querySelector(".initialList");
-    let listItems =  [].slice.call(list.children);
-     getSublists(listItems, inputs[1].value) 
-    
-
-
-  });
-
-  // this is the js code running for this job
-};
+  
+};// this is the js code running for this job
