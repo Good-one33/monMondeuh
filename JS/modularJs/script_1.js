66bdb97b3dcf15dc744d3cfd56cd741acc82347f
Abdelmalek <benali.abdelmalek63@gmail.com>
2023-06-24 12:53:16 +0200

    Job 1  done !

--- JS/modularJs/script_1.js
+++ JS/modularJs/script_1.js
@@ -11,11 +11,11 @@ let f1 = () => {
         <div class="J1_subContainer_1">
         <div class="J1_subContainer_2">
         Nbr of Items
-        <input type="number" class="J1_input" min="0" max="100" value="10">
+        <input type="number" class="J1_input" min="0" max="100" value="11">
         </div>
         <div class="J1_subContainer_2">
-        Row length
-        <input type="number" class="J1_input" min="1" max="100" value="2">
+        Row's length
+        <input type="number" class="J1_input" min="1" max="100" value="3">
         </div>
         </div>
         <div class="J1_subContainer_1">
@@ -31,7 +31,7 @@ let f1 = () => {
     let resultList = [];
     //console.log("initial List " + initialList);
     let count = 0;
-    let subListMax = subListLength; // in beggining it is equal to sublistLength but it will changes each loop 
+    let subListMax = subListLength; // in beggining it is equal to sublistLength but it will changes each loop
     let maxNbrSubList;
     if (initialList.length % subListLength != 0) {
       maxNbrSubList = Math.floor(initialList.length / subListLength) + 1;
@@ -55,59 +55,68 @@ let f1 = () => {
       resultList.push(mySubList);
     }
 
-    resultList.forEach((subList) => {
-      console.log(subList);
-    });
+    // resultList.forEach((subList) => {
+    //   console.log(subList);
+    // });
 
     return resultList;
   }
   //---------------------< insertItemsToList >----------funciton to create and insert elements to a list
-  let insertItemsToList = (classListName, nbr) => {
+  let insertItemsToList = (classListName, nbr, randomColor) => {
     let list = document.querySelector("." + classListName);
     for (let i = 1; i <= nbr; i++) {
       let listItem = document.createElement("div");
       listItem.classList.add("listItem");
+      if (randomColor) {
+        listItem.style.backgroundColor =
+          "rgb(" +
+          String(randomNumber(225)) +
+          ", " +
+          String(randomNumber(225)) +
+          "," +
+          String(randomNumber(225)) +
+          "  )";
+      }
       list.appendChild(listItem);
     }
   };
-  //---------------------< emptyList >------------------funciton remove all  elements of a list  
+  //---------------------< emptyList >------------------funciton that remove all  elements of a list
   let emptyList = (classListName) => {
     let list = document.querySelector("." + classListName);
-    let listItems =  [].slice.call(list.children);
-    listItems.forEach(child =>{
-      child.remove()
-    })
+    let listItems = [].slice.call(list.children);
+    listItems.forEach((child) => {
+      child.remove();
+    });
+  };
+  //---------------------<randomNumber>------------------funciton that generate random number between 0 and the input param
+  let randomNumber = (inputNbr) => {
+    return Math.floor(Math.random() * inputNbr + 1);
   };
 
   //¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤ main code
-  // let objectIds = ["1", "2", "1", "2", "1", "2", "1", "2", "1", "2", "1", "2"];
-  // getSublists(objectIds, 1);
   let inputs = document.querySelectorAll(".J1_input");
   let tempList = [];
-  let sublistsContainer = document.querySelector('.sublistsContainer')
+  let sublistsContainer = document.querySelector(".sublistsContainer");
+  let generateBtn = document.querySelector("button");
 
-  inputs[0].addEventListener("click", () => {
+  generateBtn.addEventListener("click", () => {
+    //sublists
     emptyList("initialList");
-    insertItemsToList("initialList",parseInt(inputs[0].value));  
-    tempList = Array.from(document.querySelector(".initialList").children)
-  });
-
-  inputs[1].addEventListener("click", () => {
+    insertItemsToList("initialList", parseInt(inputs[0].value), true);
+    tempList = Array.from(document.querySelector(".initialList").children);
     emptyList("sublistsContainer");
-    console.log('sublistsContainer : befor'+ sublistsContainer)
-    let slicedList = getSublists(tempList, parseInt(inputs[1].value)); 
-    console.log(slicedList)
-    slicedList.forEach((subList) =>{
-      const tempSubList = document.createElement('div');     
-      tempSubList.classList.add('subList');
-      for(let i=0; i<subList.length;i++){
-        tempSubList.appendChild(subList[i])
+    let slicedList = getSublists(tempList, parseInt(inputs[1].value));
+    slicedList.forEach((subList) => {
+      const tempSubList = document.createElement("div");
+      tempSubList.classList.add("subList");
+      for (let i = 0; i < subList.length; i++) {
+        tempSubList.appendChild(subList[i]);
       }
-      sublistsContainer.appendChild(tempSubList)
-    })
-    console.log('sublistsContainer : after'+ sublistsContainer)
-
+      sublistsContainer.appendChild(tempSubList);
+    });
+    //the main list
+    emptyList("initialList");
+    insertItemsToList("initialList", parseInt(inputs[0].value), false);
+    tempList = Array.from(document.querySelector(".initialList").children);
   });
-
-  
-};// this is the js code running for this job
+}; // this is the end of the  js code running for this job
