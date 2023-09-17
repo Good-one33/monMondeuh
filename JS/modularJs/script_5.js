88d9043ba0c33256a6189bebca5e9ec8143b23ad
Abdelmalek <benali.abdelmalek63@gmail.com>
2023-09-17 12:06:31 +0200

    added a spinning rectangle

--- JS/modularJs/script_5.js
+++ JS/modularJs/script_5.js
@@ -1,12 +1,34 @@
 let f5 = ()=>{
-  console.log('f6 is running')
+  console.log('f5 is running')
   // this will load the style for the job 
   document.querySelector('#jobStyle').setAttribute('href','CSS/modularCss/s'+5+'.css')   
   
   // this will load the html content
   jobDisplayDiv.innerHTML = `
-  <div> f5 is running </div>
+  <div class="container">
+  <div class="spinner"></div>
+  <div class="controller">
+    <button id="leftBtn" class="btn">Left</button>
+    <button id="rightBtn" class="btn">Right</button>
+  </div>
+</div>;
   `
   // this is the js code running for this job 
+
+  let Btns = [...document.querySelectorAll(".btn")];
+  let spinner = document.querySelector(".spinner");
+
+  jobDisplayDiv.addEventListener("click", (event) => {
+let turnNumber = getComputedStyle(spinner).getPropertyValue("--turn-number"); 
+    
+if(event.target.id === 'rightBtn'){
+    turnNumber++;
+    spinner.style.setProperty('--turn-number',turnNumber )
+}else if(event.target.id === 'leftBtn'){
+    turnNumber--;
+    spinner.style.setProperty('--turn-number',turnNumber )
+}
+});
+
 
 }////////// don't delete me 
